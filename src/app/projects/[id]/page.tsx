c2be2b2438b4d3ee7fd902db718b047be0cbe917
Juha Yoon <ijooha16@gmail.com>
2025-08-27 20:29:16 +0900

    bugfix: type 에러

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,10 +1,20 @@
 import { projects, ProjectType } from "@/data/projects";
 import ProjectDetailClient from "../_components/project-detail-client";
 
-const ProjectDetail = ({ params }: { params: { id: string } }) => {
-  const project: ProjectType = projects.find(
+interface ProjectDetailProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProjectDetail = ({ params }: ProjectDetailProps) => {
+  const project: ProjectType | undefined = projects.find(
     (p) => p.id === parseInt(params.id)
-  )!;
+  );
+
+  if (!project) {
+    return <div>프로젝트를 찾을 수 없습니다.</div>;
+  }
 
   return <ProjectDetailClient project={project} />;
 };
