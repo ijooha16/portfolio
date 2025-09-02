b862a4e823dd5a8102f9bae0fac3476c1252a49c
Juha Yoon <ijooha16@gmail.com>
2025-09-02 21:51:54 +0900

    feature/github link

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="w-screen bg-black h-32">Footer</div>;
+  return (
+    <Link href="https://github.com/ijooha16" target="_blank" rel="noreferrer">
+      <div className="flex-col gap-4 py-24 font-bold border-t-[1px] border-gray-200 flex justify-center items-center h-32">
+        {`Judy's github`}
+        <Image
+          width={42}
+          height={42}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+          alt="github"
+        />
+      </div>
+    </Link>
+  );
 };
 
 export default Footer;
